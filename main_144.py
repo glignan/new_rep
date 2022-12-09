f0f57ee0041029609ae8bd9266304b648272d7ee
glignan <glignan3@gmail.com>
2022-12-09 17:02:14 +0300

    comment11

---
+++ main_144.py
@@ -0,0 +1,65 @@
+# -*- coding: cp1251 -*-
+
+print("Привет")
+print("Привет 2")
+print("Привет 3")
+
+
+
+##############################
+# print("Введите первую точку")
+# x1 = float(input('X: '))
+# y1 = float(input('Y: '))
+# print("\nВведите вторую точку")
+# x2 = float(input('X: '))
+# y2 = float(input('Y: '))
+#
+# print("Уравнение прямой, проходящей через эти точки:")
+# x_diff = x1 - x2
+# y_diff = y1 - y2
+# if x_diff == 0:
+#     print("x = ", x1)
+# elif y_diff == 0:
+#     print("y = ", y1)
+# else:
+#     k = y_diff / x_diff
+#     b = y2 - k * x2
+#     print("y = ", k, " * x + ", b)
+
+
+
+
+
+##################################################
+# C:\Users\ROG\AppData\Local\Programs\Python\Python39\python 143Calculator_2.py
+
+# while True:
+#     command = input("Выберите операцию: ")
+#     if command in "+-*/":
+#         break
+#     print("Ошибка: такой операции не существует. Попробуйте ещё раз.")
+#
+# count = 1
+# cantidad = int(input(f"Сколько операндов? "))
+# number = int(input(f"Введите число {count}: "))
+# result_str = str(number)
+# result = number
+# cantidad -= 1
+# while cantidad != 0:
+#     cantidad -= 1
+#     count += 1
+#     number = int(input(f"Введите число {count}: "))
+#
+#
+#     if command == "+":
+#         result += number
+#     elif command == "-":
+#         result -= number
+#     elif command == "*":
+#         result *= number
+#     elif command == "/":
+#         result /= number
+#     result_str += " " + command + " " + str(number)
+#
+#
+# print(result_str + " = " + str(result))
